075d9c34ad724d60599ded7144bf1d3b79348dd8
johnpucciarelli <john.pucciarelli@gmail.com>
2016-11-14 22:26:53 -0500

    Using email text instead of html.

--- src/main/java/co/fastestpath/api/alerts/AlertFactory.java
+++ src/main/java/co/fastestpath/api/alerts/AlertFactory.java
@@ -3,7 +3,7 @@ package co.fastestpath.api.alerts;
 class AlertFactory {
 
   public static Alert from(AlertEmail email) {
-    return new Alert(email.getHtml());
+    return new Alert(email.getText());
   }
 
   private AlertFactory() {}
