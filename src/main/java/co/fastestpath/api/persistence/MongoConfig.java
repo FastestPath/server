fcc8887a507360afe966f5bb89e894925b81f38b
John Pucciarelli <john.pucciarelli@gmail.com>
2017-02-13 22:53:23 -0500

    Temporarily disabling database connection.

--- src/main/java/co/fastestpath/api/persistence/MongoConfig.java
+++ src/main/java/co/fastestpath/api/persistence/MongoConfig.java
@@ -1,23 +1,20 @@
 package co.fastestpath.api.persistence;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
-
-import com.mongodb.MongoClient;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
-@Configuration
+//@Configuration
 public class MongoConfig {
 
-    public @Bean
-    MongoDbFactory mongoDbFactory() throws Exception {
-        return new SimpleMongoDbFactory(new MongoClient(), "fastestpath");
+//    @Bean
+    public MongoDbFactory mongoDbFactory() throws Exception {
+//        return new SimpleMongoDbFactory(new MongoClient(), "fastestpath");
+      return null;
     }
 
-    public @Bean
-    MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongoDbFactory());
+//    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+//        return new MongoTemplate(mongoDbFactory());
+      return null;
     }
 }
